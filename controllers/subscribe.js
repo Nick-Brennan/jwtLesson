e907f3ff9783578cd4c61ccb8f64af47f65094c6
Nick-Brennan <nick_p_b@yahoo.com>
2016-12-14 12:59:24 -0800

    capturing returned token in header

--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -1,7 +1,10 @@
 
 function subscribe(req, res){
-  console.log(req.body);
-  res.redirect('back');
+  console.log("token", req.body.token);
+  var custHead = req.body.token;
+  res.setHeader("bearerToken", custHead);
+  res.sendStatus(200);
+  // res.writeHead(200, {'Content-Type': 'application/json', 'bearerToken': req.body.token});
 }
 
 module.exports = subscribe;
--- public/js/main.js
+++ public/js/main.js
@@ -11,4 +11,26 @@ $(function(){
 
   $('#token').val(token);
   console.log(token());
+
+  $('#myForm').submit(function(e){
+    e.preventDefault();
+
+    var myHeaders = $.ajax({
+      type: 'POST',
+      url:'/subscribe',
+      data: {token: token, subUrl: $('#subscriberUrl').val()},
+      success: function(data, textStatus, request){
+        console.log('success');
+        console.log(request);
+        console.log(this);
+        console.log(myHeaders.getAllResponseHeaders());
+        var returnedHeaders = myHeaders.getAllResponseHeaders();
+        console.log(returnedHeaders.match("bearerToken:\s(.*)"));
+        // console.log("Bearer Token: ", returnedToken);
+      },
+      error: function (request, textStatus, errorThrown) {
+        console.log("fucked");
+      }
+    });
+  });
 });
